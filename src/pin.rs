7c8e69e4fe74564a8b51196c5358e6a265070985
Thierry Kühni <thierry.kuehni@hotmail.com>
2021-10-18 12:13:18 +0200

    added polarity inversion for input pin

--- src/pin.rs
+++ src/pin.rs
@@ -1,10 +1,11 @@
 use core::cell::RefCell;
-use hal::digital::blocking::{InputPin, IoPin, OutputPin};
+use hal::digital::blocking::{InputPin, IoPin, OutputPin, StatefulOutputPin};
 use hal::digital::PinState;
 
 use super::expander::Expander;
 use super::GPIOBank;
 use super::Register;
+use super::Polarity;
 
 /// Single input device pin implementing [`InputPin`] and [`IoPin`] trait.
 ///
@@ -41,14 +42,53 @@ impl<Ex: Expander> ExpanderInputPin<Ex> {
     ///
     /// # Panics
     /// The function will panic if the provided pin is not in the allowed range of 0-7
-    pub fn new(expander: RefCell<Ex>, bank: GPIOBank, pin: u8) -> Self {
+    pub fn new(expander: RefCell<Ex>, bank: GPIOBank, pin: u8) -> Result<Self, <Ex as Expander>::Error> {
         assert!(pin < 8);
 
-        Self {
+        let register = match bank {
+            GPIOBank::Bank0 => Register::ConfigurationPort0,
+            GPIOBank::Bank1 => Register::ConfigurationPort1,
+        };
+
+        {
+            let mut expander = expander.borrow_mut();
+            let mut reg_val: u8 = 0x00;
+
+            expander.read_byte(register, &mut reg_val)?;
+
+            expander.write_byte(register, reg_val | (0x01 << pin))?;
+        }
+
+        Ok(Self {
             expander,
             bank,
             pin,
+        })
+    }
+
+    /// Sets the polarity of the input pin. The input pins have normal polarity by default on device startup.
+    /// 
+    /// If the polarity is [`Polarity::Normal`] a logic `high` voltage level on the input is detected as `high` in the software.
+    /// 
+    /// If the polarity is [`Polarity::Inverse`] a logic `high` voltage level on the input is detected as `low` by the software.
+    pub fn set_polarity(&mut self, polarity: Polarity) -> Result<(), <Ex as Expander>::Error>{
+        let register = match self.bank {
+            GPIOBank::Bank0 => Register::PolarityInversionPort0,
+            GPIOBank::Bank1 => Register::PolarityInversionPort1,
+        };
+
+        let mut expander = self.expander.borrow_mut();
+        let mut reg_val: u8 = 0x00;
+
+        expander.read_byte(register, &mut reg_val)?;
+
+        if let Polarity::Normal = polarity {
+            expander.write_byte(register, reg_val & !(0x01 << self.pin))?;
+        }else {
+            expander.write_byte(register, reg_val | (0x01 << self.pin))?;
         }
+
+        Ok(())
     }
 }
 
@@ -57,14 +97,41 @@ impl<Ex: Expander> ExpanderOutputPin<Ex> {
     ///
     /// # Panics
     /// The function will panic if the provided pin is not in the allowed range of 0-7
-    pub fn new(expander: RefCell<Ex>, bank: GPIOBank, pin: u8) -> Self {
+    pub fn new(expander: RefCell<Ex>, bank: GPIOBank, pin: u8, state: PinState) -> Result<Self, <Ex as Expander>::Error> {
         assert!(pin < 8);
 
-        Self {
+        let cp_register = match bank {
+            GPIOBank::Bank0 => Register::ConfigurationPort0,
+            GPIOBank::Bank1 => Register::ConfigurationPort1,
+        };
+
+        let op_register = match bank {
+            GPIOBank::Bank0 => Register::OutputPort0,
+            GPIOBank::Bank1 => Register::OutputPort1,
+        };
+
+        {
+            let mut expander = expander.borrow_mut();
+            let mut reg_val: u8 = 0x00;
+
+            expander.read_byte(op_register, &mut reg_val)?;
+
+            if let PinState::High = state {
+                expander.write_byte(op_register, reg_val | (0x01 << pin))?;
+            } else {
+                expander.write_byte(op_register, reg_val & !(0x01 << pin))?;
+            }
+
+            expander.read_byte(cp_register, &mut reg_val)?;
+
+            expander.write_byte(cp_register, reg_val & !(0x01 << pin))?;
+        }
+
+        Ok(Self {
             expander,
             bank,
             pin,
-        }
+        })
     }
 }
 
